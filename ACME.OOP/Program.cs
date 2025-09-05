170c6118f4b403a0602211ef32bf64e1f8aaf9b0
Angel Velasquez <pcisavel@upc.edu.pe>
2025-09-05 17:52:25 -0500

    feat(main): add main behavior.

--- ACME.OOP/Program.cs
+++ ACME.OOP/Program.cs
@@ -1,3 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using ACME.OOP.Procurement.Domain.Model.Aggregates;
+using ACME.OOP.Procurement.Domain.Model.ValueObjects;
+using ACME.OOP.SCM.Domain.Model.Aggregates;
+using ACME.OOP.SCM.Domain.Model.ValueObjects;
+using ACME.OOP.Shared.Domain.Model.ValueObjects;
 
-Console.WriteLine("Hello, World!");
+var supplierAddress = new Address(street: "Main St", number: "1213", city: "Anytown", stateOrRegion: "CA",
+    postalCode: "12345", country: "USA");
+    
+var supplier = new Supplier("SUP001", "Acme Supplies", supplierAddress);
+
+var purchaseOrder = new PurchaseOrder("PO12345", new SupplierId(supplier.Identifier),
+    DateTime.Now, "USD");
+purchaseOrder.AddItem(ProductId.New(), 10, 15.00m);
+purchaseOrder.AddItem(ProductId.New(), 5, 25.00m);
+Console.WriteLine($"order total: {purchaseOrder.CalculateOrderTotal()}");
